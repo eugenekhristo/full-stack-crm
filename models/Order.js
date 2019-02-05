731fd0e5c0689c0e1ae867c21ea4476dc8552a31
Eugene Khristo <marteau9292@gmail.com>
2019-02-05 19:07:57 +0300

    Added models for backend

---
+++ models/Order.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  order: {
+    type: Number,
+    required: true
+  },
+  category: {
+    ref: 'categories',
+    type: Schema.Types.ObjectId
+  },
+  user: {
+    ref: 'users',
+    type: Schema.Types.ObjectId
+  }
+});
+
+module.exports = mongoose.model('orders', orderSchema);
