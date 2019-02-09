60490576aeeb1ac49419051dce4a56fa23ab4c89
Eugene <eugene.khristo@mifort.org>
2019-02-09 13:02:46 +0300

    Password is hashed

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,31 @@
+const bcryptjs = require('bcryptjs');
+// models
 const User = require('../models/User');
 
 module.exports.login = (req, res) => {
 
 };
 
-module.exports.register = (req, res) => {
-  const candidate = User.findOne({emal: req.body.email});
+module.exports.register = async (req, res) => {
+  const candidate = await User.findOne({email: req.body.email});
 
   if (candidate) {
     res.status(409).json({
       message: 'Пользователь с таким email уже существует!'
     });
+  } else {
+    const password = req.body.password;
+    const salt = bcryptjs.genSaltSync();
+    const user = new User({
+      email: req.body.email,
+      password: bcryptjs.hashSync(password, salt),
+    });
+
+    try {
+      await user.save();
+      res.status(201).json(user);
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
